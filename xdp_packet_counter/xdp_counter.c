dbbd3c1aaa56a3852fc4ffa7f810d413862dc99e
Pedro Lucas Rosa <93159547+P3droLucas@users.noreply.github.com>
2024-08-24 15:23:41 -0300

    Update xdp_counter.c

--- xdp_packet_counter/xdp_counter.c
+++ xdp_packet_counter/xdp_counter.c
@@ -1,48 +1,30 @@
-#include <linux/bpf.h>
-#include <bpf/bpf_helpers.h>
-#include <linux/if_ether.h>
-#include <linux/ip.h>
-#include <linux/in.h>
+LLC ?= llc
+CLANG ?= clang
+CC ?= gcc
 
-struct {
-    __uint(type, BPF_MAP_TYPE_HASH);
-    __uint(max_entries, 256);
-    __type(key, __u32);
-    __type(value, __u64);
-} packet_count SEC(".maps");
+KERN_SOURCES := xdp_counter.c
+KERN_OBJECTS := ${KERN_SOURCES:.c=.o}
 
-SEC("xdp")
-int xdp_packet_counter(struct xdp_md *ctx) {
-    void *data_end = (void *)(long)ctx->data_end;
-    void *data = (void *)(long)ctx->data;
-    struct ethhdr *eth = data;
+CFLAGS := -g -O2 -Wall
+LDFLAGS := -lbpf -lelf
 
-    if (data + sizeof(struct ethhdr) > data_end)
-        return XDP_PASS;
+all: xdp_counter.o xdp_loader
 
-    if (eth->h_proto != __constant_htons(ETH_P_IP))
-        return XDP_PASS;
+%.o: %.c
+	$(CLANG) -S \
+	    -target bpf \
+	    -D __BPF_TRACING__ \
+	    $(CFLAGS) \
+	    -Wall \
+	    -Wno-unused-value \
+	    -Wno-pointer-sign \
+	    -Wno-compare-distinct-pointer-types \
+	    -Werror \
+	    -O2 -emit-llvm -c -g -o ${@:.o=.ll} $<
+	$(LLC) -march=bpf -filetype=obj -o $@ ${@:.o=.ll}
 
-    struct iphdr *ip = data + sizeof(struct ethhdr);
-    if ((void *)ip + sizeof(struct iphdr) > data_end)
-        return XDP_PASS;
+xdp_loader: xdp_loader.c
+	$(CC) $(CFLAGS) -o $@ $< $(LDFLAGS)
 
-	//verifica se o protocolo é TCP, UDP e ICMP
-    if(ip->protocol == IPPROTO_TCP || ip->protocol == IPPROTO_UDP || ip->protocol == IPPROTO_ICMP){
-    __u32 protocol = ip->protocol;
-    __u64 *count = bpf_map_lookup_elem(&packet_count, &protocol);
-    if (count) {
-	if (protocol == IPPROTO_ICMP && *count >= 11){
-		return XDP_DROP; //Dropando pacotes ICMP se a contagem for maior que  10.
-	}
-        __sync_fetch_and_add(count, 1);
-    } else {
-        __u64 initial_count = 1;
-        bpf_map_update_elem(&packet_count, &protocol, &initial_count, BPF_ANY);
-    }
-}
-
-    return XDP_PASS;
-}
-
-char _license[] SEC("license") = "GPL";
+clean:
+	rm -f *.o *.ll xdp_loader
